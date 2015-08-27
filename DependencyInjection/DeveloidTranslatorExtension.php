243197a4606f996cac5ef41f6c08f9342d25f229
Pavel Salauyou <pavel.salauyou@gmail.com>
2015-08-27 20:05:30 +0300

    refactoring config extension

--- DependencyInjection/DeveloidTranslatorExtension.php
+++ DependencyInjection/DeveloidTranslatorExtension.php
@@ -4,35 +4,70 @@ namespace Develoid\TranslatorBundle\DependencyInjection;
 
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\Config\FileLocator;
+use Symfony\Component\DependencyInjection\Definition;
 use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 use Symfony\Component\DependencyInjection\Loader;
 
 class DeveloidTranslatorExtension extends Extension
 {
     /**
+     * @var ContainerBuilder
+     */
+    private $container;
+
+    /**
      * {@inheritdoc}
      */
     public function load(array $configs, ContainerBuilder $container)
     {
+        $this->container = $container;
         $configuration = new Configuration();
         $config = $this->processConfiguration($configuration, $configs);
+        $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
+        $loader->load('services.yml');
 
         $container->setParameter('develoid_translator.default', $config['default']);
 
         if (isset($config['yandex'])) {
-            $container->setParameter('develoid_translator.yandex_api_key', $config['yandex']['api_key']);
+            $this->createDefinition(
+                $container->getParameter('develoid_translator_yandex_translator_class'),
+                'yandex',
+                [
+                    $config['yandex']['api_key']
+                ])
+            ;
         }
 
         if (isset($config['google'])) {
-            $container->setParameter('develoid_translator.google_api_key', $config['google']['api_key']);
+            $this->createDefinition(
+                $container->getParameter('develoid_translator_google_translator_class'),
+                'google',
+                [
+                    $config['google']['api_key']
+                ])
+            ;
         }
 
         if (isset($config['microsoft'])) {
-            $container->setParameter('develoid_translator.microsoft_client_id', $config['microsoft']['client_id']);
-            $container->setParameter('develoid_translator.microsoft_client_secret', $config['microsoft']['client_secret']);
+            $this->createDefinition(
+                $container->getParameter('develoid_translator_microsoft_translator_class'),
+                'microsoft',
+                [
+                    $config['microsoft']['client_id'],
+                    $config['microsoft']['client_secret']
+                ])
+            ;
         }
+    }
 
-        $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
-        $loader->load('services.yml');
+    /**
+     * @param $class
+     * @param $type
+     * @param array $arguments
+     */
+    private function createDefinition($class, $type, array $arguments)
+    {
+        $definition = new Definition($class, $arguments);
+        $this->container->setDefinition(sprintf('develoid_translator.%s_translator', $type), $definition);
     }
 }
